13b9ff3bd688d0afacd92926fd38122e83f8d788
OpenShift guest <ryanyuliu@gmail.com>
2013-08-06 20:06:03 -0400

    initial commit

---
+++ js/notifyCtrl.js
@@ -0,0 +1,25 @@
+function NotifyCtrl($scope, $http) {
+    var config = {headers: {"X-Parse-REST-API-Key":"pmqy17YuvRYInhxtlLM8Adm1fKsK2gj4Epxjwuhp", "X-Parse-Application-Id":"fvMcqGiCkQwp6ygypnrNpBrD24OIALuPaJiTz0qM"}};
+    $scope.button = 'Submit';
+  
+    $http.get('https://api.parse.com/1/classes/notify', config).success(function(data) {
+        console.log(data);
+        $scope.notifications = data.results;
+    });
+  
+    $scope.processForm = function (item) {
+        $scope.button = "Processing";
+        console.log(item);
+        $http.post("https://api.parse.com/1/classes/notify", item, config).success (
+            function(data) {
+                console.log(data);
+                $scope.button = "Success";
+                    $http.get('https://api.parse.com/1/classes/notify/'+data.objectId, config).success(function(data) {
+                        console.log(data);
+                        $scope.notifications.unshift(data);
+                    });
+            }
+        );
+    }
+}
+
